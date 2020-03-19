a68c60c95f3ac9d03ce5ea15732e6faa9d78c017
skylinechen <s980454@gmail.com>
2020-03-19 10:30:33 +0800

    added loop and fixed bug in script, updated makefile and README.md

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
-# Date: March 17, 2020
+# Date: March 19, 2020
 # Author: Allen CT Chen
 
 function guessinggame {
-read -p "Guessing how may file in this directory: " file_cout
-
-#wc_dir1 = `ls -l . | wc -l`
-#wcdir = wcdir1 -1
-
-if [[ "$file_cout" -gt 3 ]]
-then 
+if [ "$file_cout" -gt "$wc_dir" ];then
 	echo "The number $file_cout is too high."
-elif [[ "$file_cout" -lt 3 ]]
-then
-	echo "The number $file_cout is too low."
 else
-	echo "Congratuation! Here are 3 files under this directory!"
+	echo "The number $file_cout is too low."
 fi
 }
 
-guessinggame
+read -p "Guessing how may file in this directory: " file_cout
+
+dir=`pwd`
+wc_dir1=`ls -l $dir| wc -l`
+wc_dir=$[ $wc_dir1-1 ]
+
+while [[ "$file_cout" -ne "$wc_dir" ]]
+do
+	guessinggame
+	echo "Try to enter again!"
+	echo ""
+	read -p "Guessing how may file in this directory: " file_cout
+done
+
+echo "Congratuation! Here are $wc_dir files under this directory!"
+
